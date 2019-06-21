ef78e0301d7ff92f4d8a9454ecf99bb86bb6c2e9
David Murillo <dmz866@hotmail.com>
2019-06-21 14:30:35 -0500

    usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'usuario', component: UsuarioComponent }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
